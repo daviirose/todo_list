60ef2b8757ffb400cce75e2a7eaf9f00e4d31f89
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-03-12 16:09:57 -0400

    todo list

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,18 @@
+class TodoItemsController < ApplicationController
+before_action :set_todo_list
+	def create
+	 
+	@todo_item = @todo_list.todo_items.create(todo_item_params)
+	redirect_to @todo_list
+	end 
+
+private 
+
+	def set_todo_list
+	@todo_list = Todo_list.find(params[:todo_list_id]) 
+end
+
+	def todo_items_params
+		params[:todo_item].permit(:content)
+	end
+end
